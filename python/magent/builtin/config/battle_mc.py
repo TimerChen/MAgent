0dcbfd3c5062a6fe4be6eb30088a659869915915
timer <knosikay@gmail.com>
2018-09-12 19:43:46 +0800

    fix error in battle_mc

--- python/magent/builtin/config/battle_mc.py
+++ python/magent/builtin/config/battle_mc.py
@@ -11,8 +11,8 @@ def get_config(map_size):
     cfg.set({"minimap_mode": True})
     cfg.set({"embedding_size": 10})
 
-    multi_channel = cfg.register_agent_type(
-        "multi_channel",
+    small = cfg.register_agent_type(
+        "small",
         {'width': 1, 'length': 1, 'hp': 10, 'speed': 2,
          'view_range': gw.CircleRange(6), 'attack_range': gw.CircleRange(1.5),
          'damage': 2, 'step_recover': 0.1,
@@ -23,32 +23,14 @@ def get_config(map_size):
          'step_reward': -0.005,  'kill_reward': 5, 'dead_penalty': -0.1, 'attack_penalty': -0.1,
          })
 
-    single_channel = cfg.register_agent_type(
-        "small",
-        {'width': 1, 'length': 1, 'hp': 10, 'speed': 2,
-         'view_range': gw.CircleRange(6), 'attack_range': gw.CircleRange(1.5),
-         'damage': 2, 'step_recover': 0.1,
+    g0 = cfg.add_group(small)
+    g1 = cfg.add_group(small)
 
-         #for multi-channel communication
-         'comm_channel': 1, 'hear_all_group': False,
-
-         'step_reward': -0.005,  'kill_reward': 5, 'dead_penalty': -0.1, 'attack_penalty': -0.1,
-         })
-
-    g = []
-    g.append(cfg.add_group(multi_channel))
-    g.append(cfg.add_group(multi_channel))
-    g.append(cfg.add_group(single_channel))
-    g.append(cfg.add_group(single_channel))
-
-    a = []
-    for i in g:
-        a.append(gw.AgentSymbol(i, index='any'))
+    a = gw.AgentSymbol(g0, index='any')
+    b = gw.AgentSymbol(g1, index='any')
 
     # reward shaping to encourage attack
-    for i in a:
-        for j in a:
-            if(i!=j):
-                cfg.add_reward_rule(gw.Event(i, 'attack', j), receiver=i, value=0.2)
+    cfg.add_reward_rule(gw.Event(a, 'attack', b), receiver=a, value=0.2)
+    cfg.add_reward_rule(gw.Event(b, 'attack', a), receiver=b, value=0.2)
 
     return cfg
